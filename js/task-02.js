30ff61b2738142b3344384b67b330a10b3fe7428
Alexey Zyukov <aalex64@ukr.net>
2021-07-13 23:19:42 +0300

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -4,22 +4,85 @@
 
 // <ul id="ingredients"></ul>
 // В JS есть массив строк.
-
-// const ingredients = [
-//   'Картошка',
-//   'Грибы',
-//   'Чеснок',
-//   'Помидоры',
-//   'Зелень',
-//   'Приправы',
-// ];
 // Напиши скрипт, который для каждого элемента массива ingredients создаст 
 //отдельный li, после чего вставит все li за одну операцию в список ul.ingredients.
 //ля создания DOM - узлов используй document.createElement().
 
-//1. перебрать массив
-//2. для каждого элемента массива создать элемент li со 
-//const addLiToArray = array =>
 
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+// const addIngredients = ingredients.map(item => {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = item;
+//   listOfIngredients.append(liEl);
+//   return liEl;
+// });
+// console.log(addIngredients);
+
+const addListOfIngredients = items => {
+  return items.map(item => {
+  const liEl = document.createElement('li');
+  liEl.textContent = item;
+  listOfIngredients.append(liEl);
+  return liEl;
+  });
+}
+console.log(addListOfIngredients(ingredients));
+
+
+// const listOfIngredients = document.querySelector("#ingredients");
+// const newElem = document.createElement('li');
+// newElem.textContent = (ingredients[0]);
+// listOfIngredients.append(newElem);
+// console.log(listOfIngredients);
+
+// const addItemToList = (item, text) => {
+//   let addItem = document.createElement(item);
+//   addItem.textContent = text;
+//   listOfIngredients.append(addItem);
+// }
+// addItemToList("li", ingredients[0]);
+
+
+
+// const element = [];
+// const addElemToArray = function (array, elType) {
+//   array.forEach(element)
+//   {
+//     element.document.createElement(elType);
+//     addElement.textContent = element;
+//     listOfIngredients.append(addElement);
+//   };
+// };
+// addElemToArray(ingredients, "li")
+
+// const addElemToArray = (array, elType) => array.forEach((element) => {
+//   element.document.createElement(elType);
+//   addElement.textContent = element;
+//   listOfIngredients.append(addElement);
+// });
+// addElemToArray(ingredients, "li")
+
+
+// const list = document.querySelector(".list");
+// console.log('list', list);
+
+
+// const addItemToList = (item, text) => {
+//   let addItem = document.createElement(item);
+//   addItem.textContent = text;
+//   list.append(addItem);
+// }
+// addItemToList("li", "Картошка");
 
 
